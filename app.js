5617644bee5443a26a97264af8ebebd01a290a53
betodasilva <jrs_robert@hotmail.com>
2017-12-18 21:30:17 -0300

    add template string for new item

--- app.js
+++ app.js
@@ -71,6 +71,22 @@ var UIController = (function(){
             };
             
         },
+
+        addListItem: function(obj, type){
+            var html;
+            // create html string
+            if (type === 'inc') {
+                html = '<div class="item clearfix" id="income-%id%"><div class="item__description">%description%</div><div class="right clearfix"><div class="item__value">%value%</div><div class="item__delete"><button class="item__delete--btn"><i class="ion-ios-close-outline"></i></button></div></div></div>';
+            } else if (type === 'exp') {
+                html = '<div class="item clearfix" id="expense-%id%"><div class="item__description">%description%</div><div class="right clearfix"><div class="item__value">%value%</div><div class="item__percentage">21%</div><div class="item__delete"><button class="item__delete--btn"><i class="ion-ios-close-outline"></i></button></div></div></div>'
+            }
+            
+            // replace the placeholder text with real data
+
+            // insert the html into the DOM
+
+        },
+
         getDOMstrings: function(){
             return DOMstrings;
         }
@@ -99,6 +115,7 @@ var controller = (function(budgetCtrl, UICtrl){
         // 2. add item to budget controller
         newItem = budgetController.addItem(input.type, input.description, input.value);
         // 3. add the item to the UI
+
         // 4. calculate the budget
         // 5. display the budget on the UI
     };
