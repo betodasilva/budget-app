a65eba013b577051c7280df46cb7d29f17ca61dd
betodasilva <jrs_robert@hotmail.com>
2018-03-25 03:24:40 -0300

    add gulp setup

--- gulpfile.js
+++ gulpfile.js
@@ -2,3 +2,31 @@ const gulp = require('gulp'),
       sass = require('gulp-sass'),
       rename = require('gulp-rename'),
       browserSync = require('browser-sync').create();
+
+const src = {
+    css: {
+        dir: 'src/sass/',
+        file: 'main.scss',
+    }
+    
+}
+
+gulp.task('minify', function(){
+    gulp.src( src.css.dir + src.css.file  )
+    .pipe(sass()).on( 'error', sass.logError)
+    .pipe(rename('style.css'))
+    .pipe(gulp.dest('./'))
+    .pipe(browserSync.stream());
+});
+
+gulp.task('sass-watch', ['minify'], browserSync.reload());
+
+gulp.task('watch', function(){
+    browserSync.init({
+        server: "./"
+    });
+    gulp.watch('*.html').on('change', browserSync.reload);
+    gulp.watch(src.css.dir + '*.scss', ['minify']);
+});
+
+gulp.task('default', ['minify', 'watch']);
